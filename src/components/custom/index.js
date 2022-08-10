57db65654f610473c716165646fb32dcac010ecc
Chyyy_ <51895222+HangyuCheng@users.noreply.github.com>
2022-08-10 14:37:21 +0800

    Update index.js

--- src/components/custom/index.js
+++ src/components/custom/index.js
@@ -1,12 +1,12 @@
-import CustomPalette from './CustomPalette'
+import CustomPalette from './Custompalette'
 import CustomRenderer from './CustomRenderer'
 import CustomContextPad from './CustomContextPad'
 import MagicPropertiesProvider from './MagicPropertiesProvider'
 import NewElement1 from './NewElement1'
 
 export default {
-    __init__: ['customPalette', 'customRenderer', 'customContextPad', 'propertiesProvider','newElement1'],
-    customPalette: ['type', CustomPalette],
+    __init__: ['custompalette', 'customRenderer', 'customContextPad', 'propertiesProvider','newElement1'],
+    custompalette: ['type', Custompalette],
     newElement1:['type',NewElement1],
     customRenderer: ['type', CustomRenderer],
     customContextPad: ['type', CustomContextPad],
